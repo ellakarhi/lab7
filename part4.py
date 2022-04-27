8893fe075a3624bdd61f0ad65705afc09bb8d847
Ella Karhi <100384206+ellakarhi@users.noreply.github.com>
2022-04-27 17:18:16 +0000

    part4 is almost done

--- part4.py
+++ part4.py
@@ -1,6 +1,30 @@
-name = input("Enter name: ")
-name_length = len(name)
+def getStr(message):
+  while(True):
+    temp = input(message)
+    try:
+      temp = int(temp)
+      print("That is no the proper value")
+    except:
+      break
+  return temp
 
-for i in range (name_length):
-  print (name[i] + /t)
-  # You dont need to do this part I think it will be taken off
+def box(loop, name):
+  length = len(name)
+  for i in range(loop, length, 1):
+    print(name[i], end = ' ')
+    
+  if loop > 0:
+    for j in range(0, loop, 1):
+      print(name[j], end = ' ')
+  loop += 1
+  if loop == length:
+    print("")
+    print("Your name have now been mixed up")
+  else:
+    print("")
+    box(loop, name)
+
+name = getStr("What name would you like to get mixed up? ")
+box(0, name)
+
+
