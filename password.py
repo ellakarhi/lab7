f3e7b764669d84e34249911c4d0b7e53bf668755
Ella Karhi <100384206+ellakarhi@users.noreply.github.com>
2022-04-28 21:26:40 +0000

    Random password generator is getting worked on

--- password.py
+++ password.py
@@ -9,6 +9,9 @@ password = ""
 
 length = 4
 lower_length = random.randint(0, length)
+upper_length = ramdom.randint(0, length-lower_length)
+number_length = random.randint(0, length - upper_length - lower_length)
+symbol_length = length - number_length - upper_length- lower_length
 
 def generate_lower():
   return random.randint(0,len(lower))
@@ -22,12 +25,14 @@ for i in range (lower_length):
   password = password + lower[lower_rand]
   print (password)
 
-upper_length = length - lower_length
 for i in range (upper_length):
   upper_rand = random.randint(0, len(upper)) 
   password = password + upper[upper_rand]
 
-number_length = length - upper_length - lower_length
-
 for i in range (number_length):
-  
+  number_rand = random.randint(0, len(number))
+  password = password + number[number_rand]
+
+for i in range (symbol_length):
+  number_rand = random.randint(0, len(number))
+  password = password + number[number_rand]
